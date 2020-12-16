86e1bb8cc957d67c3ea88781c1352055baca1850
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-17 00:47:21 +0200

    finish strong password string hacker rank

--- strings/strong_password.py
+++ strings/strong_password.py
@@ -10,10 +10,6 @@ import sys
 def minimumNumber(n, password):
     # Return the minimum number of characters to make the password strong
     count_list = [1, 1, 1, 1]
-
-    if n < 6:
-        needed_char = 6 - n
-
     for single_char in password:
 
         if single_char in "0123456789":
@@ -28,12 +24,18 @@ def minimumNumber(n, password):
         elif single_char in "!@#$%^&*()-+":
             count_list[3] -= 1
 
+    # GET total number of changes needed
+
     total = 0
+    needed_char=0
     for count in count_list:
         if count <= 0:
             continue
         else:
             total += count
+    # Check if it need more character to reach 6 
+    if total + n < 6:
+        needed_char = 6 - (total + n)
 
     return total + needed_char
 
